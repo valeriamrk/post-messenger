cbf84523a5519fa1d6716e2af12714a9bf52c56a
valeriamrk <87308393+valeriamrk@users.noreply.github.com>
2023-05-03 16:46:31 +0300

    added login/logout
    added login/logout
    added user avatar

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        hi
-      </div>
+    <main className="">
+      <div className="">hi</div>
     </main>
   );
 }
